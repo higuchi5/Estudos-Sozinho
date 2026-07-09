eba6ebb08083be03b4dbeee602fcd5c71cfe5043
Murilo tadashi <Murilothiguchi@gmail.com>
2026-07-08 22:33:16 -0300

    Condições

---
+++ Backend/Js/Study/-08- Condições.js
@@ -0,0 +1,6 @@
+const velocidade = 60.5
+console.log(`A velocidade do seu carro é ${velocidade} km/h`)
+if (velocidade > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança')
---
+++ Backend/Js/Study/-09- Condição-composta.js
@@ -0,0 +1,9 @@
+const pais = 'Eua'
+console.log(`Vivendo nos ${pais}`)
+
+if ( pais == 'Brasil') {
+    console.log('Você é Brasileiro!')
+}
+else {
+    console.log('Você é Estrangeiro!')
+}
